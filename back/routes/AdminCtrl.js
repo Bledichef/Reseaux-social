ecbaa32dd715a518b3d0384e8f4d45d0137c844c
Guillaume C <81625005+Bledichef@users.noreply.github.com>
2022-03-28 14:19:23 +0200

    route AdminCtrl et lancement du front

--- back/routes/AdminCtrl.js
+++ back/routes/AdminCtrl.js
@@ -15,18 +15,17 @@ module.exports = {
     // Params
     var title = req.body.title;
     var content = req.body.content;
-    var Admin = 1;
+    //var Admin = 1;
 
     asynclib.waterfall(
       [
         function (done) {
           models.User.findOne({
             attributes: ["isAdmin"],
-            where: { isAdmin: Admin },
+            where: { id: userId },
           })
-
-            .then(function (userAdmin) {
-              done(null, userAdmin);
+            .then(function (user) {
+              done(null, user.isAdmin);
             })
             .catch(function (err) {
               return res
@@ -35,7 +34,7 @@ module.exports = {
             });
         },
         function () {
-          if (User.isAdmin == Admin) {
+          if (user.isAdmin === true) {
             update({
               title: title ? title : userFound.title,
               content: content ? content : userFound.content,
