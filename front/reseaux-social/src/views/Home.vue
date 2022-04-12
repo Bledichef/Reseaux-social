d4566611c832599e6901c4ddccc0505d29f96cb2
Guillaume C <81625005+Bledichef@users.noreply.github.com>
2022-04-12 13:17:50 +0200

    affichage des message fonctionnel

--- front/reseaux-social/src/views/Home.vue
+++ front/reseaux-social/src/views/Home.vue
@@ -1,31 +1,42 @@
 <template>
   <h1>Bienvenue</h1>
   <div class="messages">
-    <h5 class="card-header">GET Request with Async/Await</h5>
-    <div class="card-body" v-for="Message in data" :key="Message">
-      Total vue packages: {{ this.totalVuePackages }}
+    <h3 class="card-header">Derniers sujet</h3>
+    <div class="card-body" v-for="Message in messages" :key="Message">
+      <div class="Title-message">Titre message {{ Message.title }}</div>
+      <div class="content">Contenu du message {{ Message.content }}</div>
+      <div class="User">Createur du message => {{ Message.UserId }}</div>
+      <div class="creation">date création {{ Message.createdAt }}</div>
     </div>
   </div>
 </template>
 <script>
-import { mapState, store } from "vuex";
+import { mapState } from "vuex";
+
+import axios from "axios";
 
 export default {
   name: "get-request-async-await",
+
   data() {
     return {
-      Message: [],
+      messages: {},
     };
   },
   async created() {
     // Simple GET request using fetch
     const response = await fetch("http://localhost:8080/api/messages");
     const data = await response.json();
-    this.totalVuePackages = data;
+    this.messages = data;
 
-    console.log(this.totalVuePackages);
+    console.log(this.messages);
     console.log(data);
   },
 };
 </script>
-<style></style>
+<style>
+.card-body {
+  background-color: antiquewhite;
+  margin-block-end: 30px;
+}
+</style>
